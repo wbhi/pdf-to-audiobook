8726dd46a296d2df351bc69c5d255522c3af6d28
ABHIRAMA <abhiramapraneeth@gmail.com>
2026-08-06 17:45:38 +0530

    added requirements.txt

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
 import pyttsx3
 from PyPDF2 import PdfReader
 from tkinter.filedialog import askopenfilename
-
-book = askopenfilename()
-
-pdfreader = PdfReader(book)
-pages = len(pdfreader.pages)
-
-player = pyttsx3.init()
-
+book=askopenfilename()
+pdfreader=PdfReader(book)
+pages=len(pdfreader.pages)
+player=pyttsx3.init()
 for num in range(pages):
-    page = pdfreader.pages[num]
-    text = page.extract_text()
-
+    page=pdfreader.pages[num]
+    text=page.extract_text()
     if text:
         player.say(text)
-
 player.runAndWait()
